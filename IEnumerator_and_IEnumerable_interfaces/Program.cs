306137b51b09a6eea74c70a5c94242eb9cd17735
Yura <urakost427@gmail.com>
2023-02-02 16:40:44 +0200

    Add project files.

---
+++ IEnumerator_and_IEnumerable_interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IEnumerator_and_IEnumerable_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
